a823e24f25e1fc4efcddc2ea03445a9e3bfe9023
Juli Albrecht <juliella2002@gmail.com>
2025-10-24 10:45:10 +0200

    solve challenges

---
+++ react-challenges/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>Hey</p>
+      <p>Hey</p>
+    </article>
+  );
+}
