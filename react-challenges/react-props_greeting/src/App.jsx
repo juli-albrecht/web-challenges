522d65fba642f7089aabe255f38b3eb31a53ee1a
Juli Albrecht <juliella2002@gmail.com>
2025-10-28 08:24:37 +0100

    solve challenges and download solution

---
+++ react-challenges/react-props_greeting/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <p>
+      <Greeting name="Juli" />
+    </p>
+  );
+}
+
+function Greeting({ name }) {
+  const coaches = [Klaus, Felix, Roland]
+  if ()
+  return <p>Hello, {name}!</p>;
+
+}
