fd0c57e5abd9fd332993ae64df80595755b547a6
Juli Albrecht <juliella2002@gmail.com>
2025-10-30 16:34:13 +0100

    solve challenge react-effects-and-fetch_pokemon-api-with-paging

--- react-challenges/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react-challenges/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react";
 
 export default function PokemonList() {
-  const [pokemon, setPokemon] = useState([]);
-
+  const [pokemon, setPokemon] = useState();
+  const [page, setPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0"
+  );
+  console.log(page);
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(page);
         const data = await response.json();
-        setPokemon(data.results);
+        console.log(data);
+        setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
-
+  }, [page]);
+  if (!pokemon) {
+    return <p>Loading</p>;
+  }
   return (
     <main>
       <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => setPage(pokemon.next)}>
+        Next Page
+      </button>
       <ul>
-        {pokemon.map(({ name }) => (
+        {pokemon.results.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
