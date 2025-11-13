37ce29902ba2542aae01d42d2d3740fcd1b39c72
Juli Albrecht <juliella2002@gmail.com>
2025-11-13 15:25:33 +0100

    download challenge react-immutable-state

--- react-global-state_lights/pages/rooms.js
+++ react-global-state_lights/pages/rooms.js
@@ -1,12 +1,13 @@
 import Link from "../components/Link";
 import Lights from "../components/Lights";
 
-export default function Rooms({ lights, toggleLight }) {
+export default function Rooms({ lights, toggleLight, setLights }) {
+
   return (
     <>
       <Link href="/">← Back home</Link>
       <h1>All Rooms</h1>
-      <Lights lights={lights} toggleLight={toggleLight} />
+      <Lights lights={lights} setLights={setLights} toggleLight={toggleLight} />
     </>
   );
 }
