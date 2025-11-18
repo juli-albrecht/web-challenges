0d3a253e7adc8fe752743d7d62806690af81ff85
Juli Albrecht <juliella2002@gmail.com>
2025-11-18 10:08:35 +0100

    download solutions etc.

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,45 +1,61 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
+import { SWRConfig } from "swr";
+
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+async function fetcher(URL) {
+  const response = await fetch(URL);
+  return await response.json();
+}
+
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+const { data, error, isLoading, mutate } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+console.log(data)
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+if (error) return <div>failed to load</div>; console.log("loading");
+  if (isLoading) return <div>Loading...</div>;
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  // const [coords, setCoords] = useState({
+  //   longitude: 0,
+  //   latitude: 0,
+  // });
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  // async function getISSCoords() {
+  //   try {
+  //     const response = await fetch(URL);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     getISSCoords();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <SWRConfig value={{fetcher}}>
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
+      </SWRConfig>
     </main>
   );
 }
